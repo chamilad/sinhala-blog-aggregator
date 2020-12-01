4c1fba5d78f7e323e2e6d27357fb23d9f4973fb2
Chamila de Alwis <chamila.sujeewa@gmail.com>
2020-12-01 23:06:35 +1300

    Cleanup before adding the hugo site

--- README.md
+++
@@ -1,2 +0,0 @@
-# sinhala-blog-aggregator
-an aggregator site for RSS and Atom feeds to be used for Sinhala blogs of interest
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/chamilad/sinhala-blog-aggregator/cmd"
-)
-
-func main() {
-	cmd.Execute()
-}
